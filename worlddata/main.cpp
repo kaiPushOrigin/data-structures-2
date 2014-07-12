7958ae59f8d817a05d54794785c33a20a7bb0565
Kashif <kashiv.agnelite@gmail.com>
2014-07-12 15:57:25 -0300

    First commit

---
+++ worlddata/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  worlddata
+//
+//  Created by Kai on 2014-07-12.
+//  Copyright (c) 2014 world data assignment. All rights reserved.
+//
+
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
+
