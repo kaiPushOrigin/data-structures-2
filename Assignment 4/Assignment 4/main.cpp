a513385ca02533497d5117ebee2bf5f541ba193e
Kashif <kashiv.agnelite@gmail.com>
2014-08-12 01:07:21 -0300

    Edited according to Assignment

--- Assignment 4/Assignment 4/main.cpp
+++ Assignment 4/Assignment 4/main.cpp
@@ -20,17 +20,17 @@ public:
 
 struct TwoThreeTree
 {
-    TwoThreeTree *pChild1;
-    TwoThreeTree *pChild2;
-    TwoThreeTree *pChild3;
-    Babies RefLo;
-    Babies RefHi;
+    TwoThreeTree *leftChildPtr;
+    TwoThreeTree *midChildPtr;
+    TwoThreeTree *rightChildPtr;
+    Babies smallItem;
+    Babies largeItem;
 };
 
-bool InsertRec(TwoThreeTree *pNode, Babies &baby, TwoThreeTree **ppN1,TwoThreeTree **ppN2)
+bool InsertRec(TwoThreeTree *parentNode, Babies &baby, TwoThreeTree **ppN1,TwoThreeTree **ppN2)
 {
-    TwoThreeTree *pN1  = NULL;
-    TwoThreeTree *pN2  = NULL;
+    TwoThreeTree *newNode1  = NULL;
+    TwoThreeTree *newNode2  = NULL;
     TwoThreeTree *pNew = NULL;
     
     Babies key[3];           // If split occurs, turn a 3-node into a 4-node
@@ -39,130 +39,133 @@ bool InsertRec(TwoThreeTree *pNode, Babies &baby, TwoThreeTree **ppN1,TwoThreeTr
     
     
     // Internal node.  Recurses down to the appropriate child. If the recursion caused a split, finish the split logic here.
-    if (pNode->pChild1 != NULL)
+    if (parentNode->leftChildPtr != NULL)
     {
-        if (baby.name < pNode->RefLo.name) // Recurse down the first child.
+        if (baby.name < parentNode->smallItem.name) // Recurse down the first child.
         {
-            if (InsertRec(pNode->pChild1, baby, &pN1, &pN2) == false)
+            if (InsertRec(parentNode->leftChildPtr, baby, &newNode1, &newNode2) == false)
                 return false;
             
             // A split of the child nodes was performed. If this is a 2-node, we just need to rearrange the keys and child pointers.
-            if (NULL == pNode->RefHi.pContext)              // shift the second child over to become the third child.
+            if (NULL == parentNode->largeItem.pContext)              // shift the second child over to become the third child.
             {
-                pNode->RefHi   = pNode->RefLo;
-                pNode->RefLo   = baby;
-                pNode->pChild3 = pNode->pChild2;      // In this case, the splitting is now contained, so we return false
-                pNode->pChild2 = pN2;
-                pNode->pChild1 = pN1;
+                parentNode->largeItem   = parentNode->smallItem;
+                parentNode->smallItem   = baby;
+                parentNode->rightChildPtr = parentNode->midChildPtr;      // In this case, the splitting is now contained, so we return false
+                parentNode->midChildPtr = newNode2;
+                parentNode->leftChildPtr = newNode1;
                 return false;
             }
             else // Convert this 3-node into a 4-node.
             {
                 key[0] = baby;
-                key[1] = pNode->RefLo;
-                key[2] = pNode->RefHi;
-                pC[0]  = pN1;
-                pC[1]  = pN2;
-                pC[2]  = pNode->pChild2;
-                pC[3]  = pNode->pChild3;
+                key[1] = parentNode->smallItem;
+                key[2] = parentNode->largeItem;
+                pC[0]  = newNode1;
+                pC[1]  = newNode2;
+                pC[2]  = parentNode->midChildPtr;
+                pC[3]  = parentNode->rightChildPtr;
             }
         }
         
-        else if (NULL != pNode->RefHi.pContext)    // If this is a 3-node, we need to decide whether to recurse down the second or third child.
+        else if (NULL != parentNode->largeItem.pContext)    // If this is a 3-node, we need to decide whether to recurse down the second or third child.
         {
-            if (baby.name < pNode->RefHi.name)  // Recurse down the middle child.
+            if (baby.name < parentNode->largeItem.name)  // Recurse down the middle child.
             {
-                if (false == InsertRec(pNode->pChild2, baby, &pN1, &pN2))
+                if (false == InsertRec(parentNode->midChildPtr, baby, &newNode1, &newNode2))
                     return false;
                 
-                key[0] = pNode->RefLo;   // The recursive call caused a split.  As we now have a 4-node, split this node as well.
+                key[0] = parentNode->smallItem;   // The recursive call caused a split.  As we now have a 4-node, split this node as well.
                 key[1] = baby;
-                key[2] = pNode->RefHi;
-                pC[0]  = pNode->pChild1;
-                pC[1]  = pN1;
-                pC[2]  = pN2;
-                pC[3]  = pNode->pChild3;
+                key[2] = parentNode->largeItem;
+                pC[0]  = parentNode->leftChildPtr;
+                pC[1]  = newNode1;
+                pC[2]  = newNode2;
+                pC[3]  = parentNode->rightChildPtr;
             }
-            else if (baby.name > pNode->RefHi.name)       // Recurse down the third child.
+            else if (baby.name > parentNode->largeItem.name)       // Recurse down the third child.
             {
-                if (false == InsertRec(pNode->pChild3, baby, &pN1, &pN2))
+                if (false == InsertRec(parentNode->rightChildPtr, baby, &newNode1, &newNode2))
                     return false;
                 
-                key[0] = pNode->RefLo;
-                key[1] = pNode->RefHi;
+                key[0] = parentNode->smallItem;
+                key[1] = parentNode->largeItem;
                 key[2] = baby;
-                pC[0]  = pNode->pChild1;
-                pC[1]  = pNode->pChild2;
-                pC[2]  = pN1;
-                pC[3]  = pN2;
+                pC[0]  = parentNode->leftChildPtr;
+                pC[1]  = parentNode->midChildPtr;
+                pC[2]  = newNode1;
+                pC[3]  = newNode2;
             }
             else
             {
-                pNode->RefHi = baby;
+                parentNode->largeItem = baby;
                 return false;
             }
         }
-        else if (baby.name > pNode->RefLo.name)          // Otherwise this is a 2-node, so recurse down the middle child.
+        else if (baby.name > parentNode->smallItem.name)          // Otherwise this is a 2-node, so recurse down the middle child.
         {
-            if (InsertRec(pNode->pChild2, baby, &pN1, &pN2) == false)
+            if (InsertRec(parentNode->midChildPtr, baby, &newNode1, &newNode2) == false)
                 return false;
             
-            pNode->RefHi   = baby;       // Split occured. Replace the current key and pointers with the values returned from recursive call
-            pNode->pChild2 = pN1;
-            pNode->pChild3 = pN2;
+            parentNode->largeItem   = baby;       // Split occured. Replace the current key and pointers with the values returned from recursive call
+            parentNode->midChildPtr = newNode1;
+            parentNode->rightChildPtr = newNode2;
             return false;
         }
         
         else
         {
-            pNode->RefLo = baby;
+            parentNode->smallItem = baby;
             return false;
         }
     }
     
     else             // Leaf node. No children. Insert the new key in this node
     {
-        if (NULL == pNode->RefHi.pContext)  //This leaf only contains one key. No split required. Insert the new key into the correct place
+        if (NULL == parentNode->largeItem.pContext)  //This leaf only contains one key. No split required. Insert the new key into the correct place
         {
-            if (baby.name > pNode->RefLo.name)    // If the new key is bigger than the one key in this node, it becomes the second key.
-                pNode->RefHi = baby;
-            else if (baby.name < pNode->RefLo.name)// Shift the existing key over to be the second key, then drop the new key in as the first key.
+            if (baby.name > parentNode->smallItem.name)    // If the new key is bigger than the one key in this node, it becomes the second key.
+                parentNode->largeItem = baby;
+            else if (baby.name < parentNode->smallItem.name)// Shift the existing key over to be the second key, then drop the new key in as the first key.
             {
-                pNode->RefHi = pNode->RefLo;
-                pNode->RefLo = baby;
+                parentNode->largeItem = parentNode->smallItem;
+                parentNode->smallItem = baby;
             }
             else                                 // Replace an existing key with a new value.
-                pNode->RefLo = baby;
+                parentNode->smallItem = baby;
 
             return false;                        // No split required.
         }
         
-        if (baby.name < pNode->RefLo.name)    // Otherwise splitting is required. Rearrange the existing keys so that they are correctly sorted.
+        if (baby.name < parentNode->smallItem.name)    // Otherwise splitting is required. Rearrange the existing keys so that they are correctly sorted.
         {
             key[0] = baby;
-            key[1] = pNode->RefLo;
-            key[2] = pNode->RefHi;
+            key[1] = parentNode->smallItem;
+            key[2] = parentNode->largeItem;
         }
-        else if (baby.name == pNode->RefLo.name)   // Special case: replace an existing value.
+        else if (baby.name == parentNode->smallItem.name)   // Special case: replace an existing value.
         {
-            pNode->RefLo = baby;
+            parentNode->smallItem = baby;
+            cout << "small item " << parentNode->smallItem.name << endl;
             return false;
         }
-        else if (baby.name < pNode->RefHi.name)
+        else if (baby.name < parentNode->largeItem.name)
         {
-            key[0] = pNode->RefLo;
+            key[0] = parentNode->smallItem;
             key[1] = baby;
-            key[2] = pNode->RefHi;
+            key[2] = parentNode->largeItem;
         }
-        else if (baby.name == pNode->RefHi.name)  // Special case: replace an existing value.
+        else if (baby.name == parentNode->largeItem.name)  // Special case: replace an existing value.
         {
-            pNode->RefHi = baby;
+            parentNode->largeItem = baby;
+            cout << "large item " << parentNode->largeItem.name << endl;
+
             return false;
         }
         else
         {
-            key[0] = pNode->RefLo;
-            key[1] = pNode->RefHi;
+            key[0] = parentNode->smallItem;
+            key[1] = parentNode->largeItem;
             key[2] = baby;
         }
         pC[0] = pC[1] = pC[2] = pC[3] = NULL;  // This is a leaf node, leave all child pointers NULL.
@@ -171,21 +174,21 @@ bool InsertRec(TwoThreeTree *pNode, Babies &baby, TwoThreeTree **ppN1,TwoThreeTr
     // The remaining code is only executed if we have created a 4-node. This forces pNode to split.
     // The code above has already stored the keys and child pointers in the correct order.
     
-    pNode->RefLo          = key[0];   // Replace the contents of pNode to contain the first two child nodes.
-    pNode->RefHi.name     = "";
-    pNode->RefHi.pContext = NULL;
-    pNode->pChild1        = pC[0];
-    pNode->pChild2        = pC[1];
-    pNode->pChild3        = NULL;
+    parentNode->smallItem          = key[0];   // Replace the contents of pNode to contain the first two child nodes.
+    parentNode->largeItem.name     = "";
+    parentNode->largeItem.pContext = NULL;
+    parentNode->leftChildPtr        = pC[0];
+    parentNode->midChildPtr        = pC[1];
+    parentNode->rightChildPtr        = NULL;
     
     pNew = new TwoThreeTree();       // Create a new node to contain the last two child pointers.
-    pNew->RefLo           = key[2];
-    pNew->pChild1         = pC[2];
-    pNew->pChild2         = pC[3];
-    pNew->pChild3         = NULL;
+    pNew->smallItem           = key[2];
+    pNew->leftChildPtr         = pC[2];
+    pNew->midChildPtr         = pC[3];
+    pNew->rightChildPtr         = NULL;
     
     baby   = key[1];     // Return the info about these two sibling nodes for splitting it in Insert Function
-    *ppN1 = pNode;
+    *ppN1 = parentNode;
     *ppN2 = pNew;
     return true;
 }
@@ -200,15 +203,15 @@ TwoThreeTree *Insert(Babies baby, TwoThreeTree *Tree)
         if (InsertRec(Tree, baby, &pN1, &pN2))  // If InsertRec is true, split the root of the tree
         {                                       // pN1 is the current root pointer, a new node is returned in pN2
             Tree = new TwoThreeTree();          // baby is passed by reference, it contains the key for the new root
-            Tree->RefLo   = baby;
-            Tree->pChild1 = pN1;
-            Tree->pChild2 = pN2;
+            Tree->smallItem   = baby;
+            Tree->leftChildPtr = pN1;
+            Tree->midChildPtr = pN2;
         }
     }
     else    // Insert into an empty tree
     {
         Tree = new TwoThreeTree();
-        Tree->RefLo  = baby;
+        Tree->smallItem  = baby;
     }
     return (Tree);
 }
@@ -216,49 +219,49 @@ TwoThreeTree *Insert(Babies baby, TwoThreeTree *Tree)
 
 void inorder(TwoThreeTree *copyTree)
 {
-    if((copyTree->pChild1 == NULL) && (copyTree->pChild2 == NULL) && (copyTree->pChild3 == NULL))
-        cout << copyTree->RefLo.name << " " << copyTree->RefLo.uses << " " << copyTree->RefLo.rank << endl;
+    if((copyTree->leftChildPtr == NULL) && (copyTree->midChildPtr == NULL) && (copyTree->rightChildPtr == NULL))
+        cout << copyTree->smallItem.name << " " << copyTree->smallItem.uses << " " << copyTree->smallItem.rank << endl;
     
-    else if((copyTree->RefLo.name != "") && (copyTree->RefHi.name != ""))
+    else if((copyTree->smallItem.name != "") && (copyTree->largeItem.name != ""))
     {
-        inorder(copyTree->pChild1);
-        cout << copyTree->RefLo.name << " " << copyTree->RefLo.uses << " " << copyTree->RefLo.rank << endl;
-        inorder(copyTree->pChild2);
-        cout << copyTree->RefHi.name << " " << copyTree->RefHi.uses << " " << copyTree->RefHi.rank << endl;
-        inorder(copyTree->pChild3);
+        inorder(copyTree->leftChildPtr);
+        cout << copyTree->smallItem.name << " " << copyTree->smallItem.uses << " " << copyTree->smallItem.rank << endl;
+        inorder(copyTree->midChildPtr);
+        cout << copyTree->largeItem.name << " " << copyTree->largeItem.uses << " " << copyTree->largeItem.rank << endl;
+        inorder(copyTree->rightChildPtr);
     }
     
     else
     {
-        inorder(copyTree->pChild1);
-        cout << copyTree->RefLo.name << " " << copyTree->RefLo.uses << " " << copyTree->RefLo.rank << endl;
-        inorder(copyTree->pChild2);
+        inorder(copyTree->leftChildPtr);
+        cout << copyTree->smallItem.name << " " << copyTree->smallItem.uses << " " << copyTree->smallItem.rank << endl;
+        inorder(copyTree->midChildPtr);
     }
 }
 
 TwoThreeTree* findItem(TwoThreeTree* Tree, string key)
 {
-    if(key == Tree->RefLo.name)
+    if(key == Tree->smallItem.name)
         return Tree;
     
-    else if((Tree->pChild1 == NULL) && (Tree->pChild2 == NULL) && (Tree->pChild3 == NULL))
+    else if((Tree->leftChildPtr == NULL) && (Tree->midChildPtr == NULL) && (Tree->rightChildPtr == NULL))
         return NULL;
     
-    else if((Tree->RefLo.name != "") && (Tree->RefHi.name != ""))
+    else if((Tree->smallItem.name != "") && (Tree->largeItem.name != ""))
     {
-        if(key < Tree->RefLo.name)
-            return findItem(Tree->pChild1, key);
-        else if(key < Tree->RefLo.name)
-            return findItem(Tree->pChild2, key);
+        if(key < Tree->smallItem.name)
+            return findItem(Tree->leftChildPtr, key);
+        else if(key < Tree->smallItem.name)
+            return findItem(Tree->midChildPtr, key);
         else
-            return findItem(Tree->pChild3, key);
+            return findItem(Tree->rightChildPtr, key);
     }
     else
     {
-        if(key < Tree->RefLo.name)
-            return findItem(Tree->pChild1, key);
+        if(key < Tree->smallItem.name)
+            return findItem(Tree->leftChildPtr, key);
         else
-            return findItem(Tree->pChild2, key);
+            return findItem(Tree->midChildPtr, key);
     }
 }
 
@@ -271,7 +274,7 @@ int main (int argc, char** argv)
     
 	string line, rank, name, uses;
 	int count = 1;
-	int femaleBabiesIndex = 0;
+	int BabiesIndex = 0;
 	while (getline(infile, line))
 	{
         copyTree = new TwoThreeTree();
@@ -286,39 +289,48 @@ int main (int argc, char** argv)
                 baby.name = name.substr(0, posn);
                 baby.uses = name.substr(posn+1);
                 Tree = Insert(baby, Tree);
-                femaleBabiesIndex++; count++;
+                BabiesIndex++; count++;
                 break;
                 
             }
 		    case 2:
             {
-                size_t pos = line.find(" ");
-                baby.rank = line.substr(0, pos);                     // Value before first whitespace
-                name = line.substr(pos+1);                           // name = whole sentence after first whitespace
-                size_t posn = name.find(" ");                               // i.e second white space
-                baby.name = name.substr(0, posn);
-                baby.uses = name.substr(posn+1);
-                Tree = Insert(baby, Tree);
-                femaleBabiesIndex++;
+                //size_t pos = line.find(" ");
+                //baby.rank = line.substr(0, pos);                     // Value before first whitespace
+                //name = line.substr(pos+1);                           // name = whole sentence after first whitespace
+                //size_t posn = name.find(" ");                               // i.e second white space
+                //baby.name = name.substr(0, posn);
+                //baby.uses = name.substr(posn+1);
+                //Tree = Insert(baby, Tree);
+                BabiesIndex++;
                 count = 1;
                 break;
             }
 	    }
 	}
-    string key;
-    inorder(Tree);
+    cout << "Initializing from babies.txt: " << BabiesIndex << "lines/records" << endl;
+    string key = "";
+    //inorder(Tree);
     
-    cout << "\n\n Search: ";
+    do
+    {
+    cout << "\nPlease enter a name of interest: ";
     cin >> key;
-    
+    if(key == "QUIT")
+    {
+        cout << "Thank you for using popularity";
+        break;
+    }
     copyTree = findItem(Tree, key);
     if (copyTree == NULL)
     {
-        cout << "No data found for " << key << "\n\n";
+        cout << key << " was not in the top 100 male or female names" << endl;
     }
     else
     {
-        cout << copyTree->RefLo.name << " " << copyTree->RefLo.uses;
+        cout << copyTree->smallItem.name << " is the " << copyTree->smallItem.rank << "th name (" << copyTree->smallItem.uses
+        << " uses in 2013)";
     }
+    }while(true);
 	
 }
